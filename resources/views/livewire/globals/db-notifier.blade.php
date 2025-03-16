b8b3848fc6b2cb2544bec0f7e1694de1abb305d5
Eunice T. <eunicetemfack1@gmail.com>
2025-03-16 19:21:49 +0100

    update db-notifier

--- resources/views/livewire/globals/db-notifier.blade.php
+++ resources/views/livewire/globals/db-notifier.blade.php
@@ -35,9 +35,7 @@ new class extends Component {
      @notification-read.window="$wire.$refresh()"
      @notifications-cleared.window="show_notifications = false; $wire.$refresh()">
     <button @click="show_notifications = !show_notifications" class="flex items-center relative p-0.5 bg-primary-800 text-white hover:bg-secondary-800  rounded-full transition-colors">
-        <svg class="w-6 h-6 sm:w-6 sm:h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m10.827 5.465-.435-2.324m.435 2.324a5.338 5.338 0 0 1 6.033 4.333l.331 1.769c.44 2.345 2.383 2.588 2.6 3.761.11.586.22 1.171-.31 1.271l-12.7 2.377c-.529.099-.639-.488-.749-1.074C5.813 16.73 7.538 15.8 7.1 13.455c-.219-1.169.218 1.162-.33-1.769a5.338 5.338 0 0 1 4.058-6.221Zm-7.046 4.41c.143-1.877.822-3.461 2.086-4.856m2.646 13.633a3.472 3.472 0 0 0 6.728-.777l.09-.5-6.818 1.277Z"/>
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 sm:w-6 sm:h-6" viewBox="0 0 50 50"><g fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path stroke="#344054" d="M31.25 37.5h-12.5a6.25 6.25 0 0 0 12.5 0"/><path stroke="#306cfe" d="M40.375 29.958a4.42 4.42 0 0 1 1.292 3.125v0A4.417 4.417 0 0 1 37.25 37.5h-24.5a4.416 4.416 0 0 1-4.417-4.417v0a4.42 4.42 0 0 1 1.292-3.125l2.875-2.875V18.75A12.5 12.5 0 0 1 25 6.25v0a12.5 12.5 0 0 1 12.5 12.5v8.333z"/></g></svg>
         @if($this->notifications->isNotEmpty())
             <span class="absolute top-0 right-0 sm:-top-1 sm:-right-1 bg-red-500 text-white text-xxs font-bold rounded-full h-4 w-4 sm:h-4 sm:w-4 flex items-center justify-center">
                     {{ $this->notifications->count() }}
@@ -45,7 +43,7 @@ new class extends Component {
         @endif
     </button>
 
-    <div class="absolute w-60 md:w-96 p-1 right-2 -bottom-4 translate-y-full shadow-lg shadow-black bg-white z-20 rounded-lg max-h-96 overflow-y-auto" x-show="show_notifications" @click.outside="show_notifications=false">
+    <div class="absolute w-60 md:w-96 p-1 right-2 -bottom-4 translate-y-full shadow-lg shadow-black bg-white z-20 rounded-lg max-h-96 overflow-y-auto bg-surface dark:divide-outline-dark dark:border-outline-dark dark:bg-surface-dark rounded-radius dark:text-white" x-show="show_notifications" @click.outside="show_notifications=false">
         <div>
             <div class="flex justify-between items-center p-2 border-b-2">
                 @if($this->notifications->isNotEmpty())
@@ -55,7 +53,7 @@ new class extends Component {
             </div>
             @forelse($this->notifications as $notification)
                 <div class="bg-secondary-100 text-sm p-4 m-2">
-                    <p class="text-black font-semibold">{{ $notification->data['message'] }}</p>
+                    <p class=" font-semibold">{{ $notification->data['message'] }}</p>
 
                     <button class="btn-ghost-primary !text-xs mt-1" wire:click="markAsRead('{{ $notification->id }}')">
                         {{__('Mark as read')}}
